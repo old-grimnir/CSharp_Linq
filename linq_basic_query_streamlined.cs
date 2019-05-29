8de98b4c709d298115df629a7b444d035d9d97da
old-grimnir <49686783+old-grimnir@users.noreply.github.com>
2019-05-29 13:58:55 +0100

    Add files via upload

---
+++ linq_basic_query_streamlined.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scratchname
+{
+    class Scratch
+    {
+        static void Main(string[] args)
+        {
+            Int32 vals = Enumerable.Range(1, 999).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+            Console.WriteLine("Answer: {0}", vals);
+        }
+    }
+}
