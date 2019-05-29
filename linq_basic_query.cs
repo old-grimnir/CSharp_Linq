e660dfd2b7501a798ea3bba630c3d6529c9f7625
old-grimnir <49686783+old-grimnir@users.noreply.github.com>
2019-05-29 13:54:16 +0100

    Add files via upload

---
+++ linq_basic_query.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scratchname
+{
+    class Scratch
+    {
+        static void Main(string[] args)
+        {
+            int[] vals = Enumerable.Range(1, 999).ToArray(); // array comprehension
+            int total = 0;
+
+            var lq = from v in vals  // actual linq query
+                     where (v % 3 == 0) || (v % 5 == 0)
+                     select v;
+
+            foreach (var sv in lq) // use the linq object
+            {
+                total += sv;
+            };
+
+            Console.WriteLine("Answer: {0}", total);
+        }
+    }
+}
